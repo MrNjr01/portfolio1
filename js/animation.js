c392161dfb989d3cc5b0a4f2f6da43dec69646d4
MrNjr01 <129020914+MrNjr01@users.noreply.github.com>
2024-10-26 00:40:17 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["S Swoyam", "Problem solver", "Consultant", "Freelancer" ];  // "Web Developer",
+const textArray = ["Swoyam", "Problem solver", "Consultant", "Freelancer"];  // "Web Developer",
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
